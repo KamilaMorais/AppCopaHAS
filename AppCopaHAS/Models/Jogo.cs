b1da59170485ce1b6162d6fbf2d98f4ea4a7b5e0
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-06-01 09:45:33 -0300

    Aplicativo - parte visual 01/06

---
+++ AppCopaHAS/Models/Jogo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCopaHAS.Models
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public int EstadioId { get; set; }
+    }
+}
